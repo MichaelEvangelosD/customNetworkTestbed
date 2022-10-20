9119567a7c5facb0ffa07fe3badc50c6f10f163c
Michael Evangelos Diamantis <92045542+MichaelEvangelosD@users.noreply.github.com>
2022-10-20 15:02:42 +0300

    Initial project commit

---
+++ CustomNetworking/CustomNetworkTestbed/CustomNetworkTestbed.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CustomNetworkTestbed
+{
+    class CustomNetworkTestbed
+    {
+        public static int Main(string[] args)
+        {
+            if(args.Length < 2)
+            {
+                Console.WriteLine("Less passed arguments than needed!");
+                return 1;
+            }
+
+            string passedIPAddress = args[0];
+            string passedPort = args[1];
+
+            //Parsing passed arguments to respected types.
+            IPAddress parsedIPAddress = IPAddress.Parse(passedIPAddress);
+            int parsedPort = int.Parse(passedPort);
+
+            //TODO: RUN()
+
+            return 0;
+        }
+    }
+}
